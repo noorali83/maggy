ac73f482710377b46eb4099ce585f792ed1f1b6f
Noor Ali <noorali83@gmail.com>
2018-11-21 21:15:52 +1100

    Added checkout endpoint

--- application.py
+++ application.py
@@ -1,15 +1,35 @@
-from os import abort
+from decimal import Decimal
+
 from application import db, application
-from application.models import Transaction, Card, School, User, UserSchema
+from application.models import Card, User, UserSchema, School, CardSchema
 from flask import request, jsonify
 
 
-@application.route('/checkout', methods=['POST'])
+@application.route('/api/checkout', methods=['POST'])
 def checkout():
-    if not request.json:
-        abort(400)
-    data = request.get_json(force=True)
-    return jsonify(request.get_json(force=True))
+    req_data = request.get_json()
+
+    cardNumber = req_data['cardNumber']
+    amount = req_data['amount']
+    amount_in_decimal = Decimal(amount.replace(',', '.'))
+    card = Card.query.filter_by(number=cardNumber).first()
+
+    error = None
+    status = None
+
+    if card is not None:
+        if card.balance >= amount_in_decimal:
+            card.balance = card.balance - amount
+            db.session.commit()
+            status = 'Approved'
+        else:
+            status = 'Declined'
+            error = 'Insufficient balance, Please topup your card'
+    else:
+        error = 'Card not found'
+        status = 'Card not found'
+
+    return jsonify(status = status, error = error)
 
 
 @application.route('/api/users', methods=['GET'])
@@ -18,5 +38,12 @@ def users():
     users = UserSchema(many=True).dump(users).data
     return jsonify(users)
 
+@application.route('/api/cards', methods=['GET'])
+def cards():
+    cards = Card.query.limit(100).all()
+    cards = CardSchema(many=True).dump(cards).data
+    return jsonify(cards)
+
+
 if __name__ == '__main__':
-    application.run(host='127.0.0.1', port=5001, debug=True)
+    application.run(host='localhost', port=5001, debug=True)
